59de46e9172342c27cca4c67d066a1fef734d23c
underbleu <underbleu.dev@gmail.com>
2018-11-16 00:45:43 +0900

    webpack setting for client

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-const express = require('express')
-const React = require('react')
-const renderToString = require('react-dom/server').renderToString
-const Home = require('./client/components/Home').default
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import Home from './client/components/Home'
+
 const app = express()
 
+// Set "public" as a static file directory
+// then, we don't need to specify directory on <script src="bundle.js">
+app.use(express.static('public'))
 app.get('/', (requestAnimationFrame, res) => {
   // Render as a string
   const content = renderToString(<Home />)
-  res.send(content)
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div>${content}</div>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
+  `
+  res.send(html)
 })
 
 app.listen(3000, () => {

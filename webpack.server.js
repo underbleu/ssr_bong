7d7ff864958a06c76aa7dc35d80754096d6fa4e7
underbleu <underbleu.dev@gmail.com>
2018-11-16 15:08:54 +0900

    Ignore files with webpack for server bundle.js because node-environment use node_modules

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
   // Inform webpack that we're building a bundle
@@ -10,15 +11,19 @@ const config = {
   // Tell webpack the root file of our server application
   entry: './src/index.js',
 
-  // Tell webpack where to put the output file
-  // that is generated
+  // Tell webpack where to put the output file that is generated
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
     // path: path module helper(nodeJS)
     // __dirname: reference to the current working directory the our project is being excuted in
     // 'build' directory: will be generated automatically
-  }
+  },
+
+  // Anything(library codes...) that's inside the node_modules will not be included of our server-bundle.js file
+  // Because node-environment use node_modules unlike browser-environment
+  // So server-bundle.js will be smaller and webpack process will be faster
+  externals: [webpackNodeExternals()],
 }
 
 module.exports = merge(baseConfig, config)

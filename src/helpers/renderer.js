4f8543e0992cc9a24b9508af4a0ab9e04b57de9c
underbleu <underbleu.dev@gmail.com>
2018-11-16 17:18:12 +0900

    Server-side routing setup

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import Home from '../client/components/Home'
+import { StaticRouter } from 'react-router-dom'
+import Routes from '../client/Routes'
 
-// Seperate rendering logic for React-app with 
-// to help clarify server process
-export default () => {
-  const content = renderToString(<Home />)
+// renderer.js: Seperate React rendering logic with server-side logic to help clarify server process
+// context: A plain JavaScript object. During the render, components can add properties to the object to store information about the render (Redirects, Error handling)
+// location: BrowserRouter can know current path, but StaticRouter need to be told what the current path is from request
+export default (req) => {
+  const content = renderToString(
+    <StaticRouter location={req.path} context={{}}>
+      <Routes />
+    </StaticRouter>
+  )
 
   return `
     <html>
--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const app = express()
 // Set "public" as a static file directory
 // then, we don't need to specify directory on <script src="bundle.js">
 app.use(express.static('public'))
-app.get('/', (requestAnimationFrame, res) => {
-  res.send(renderer())
+app.get('/', (req, res) => {
+  res.send(renderer(req)) // req: Let StaticRouter know current path
 })
 
 app.listen(3000, () => {

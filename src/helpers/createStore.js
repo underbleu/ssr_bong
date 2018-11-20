83c5e7d67d7e2e031812887afcd04e7b5aa818ea
underbleu <underbleu.dev@gmail.com>
2018-11-20 21:18:05 +0900

    Create store, reducers

---
+++ src/helpers/createStore.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from '../client/reducers'
+
+export default () => {
+  const store = createStore(reducers, {}, applyMiddleware(thunk))
+
+  return store
+}

01bf8d7aa98d266e456c8ae9696c20fbc5ea97c6
underbleu <underbleu.dev@gmail.com>
2018-11-21 21:40:52 +0900

    Mitigating XSS Attack - serialize: Replace any special-characters with Unicode (ex. <script>)

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -3,6 +3,7 @@ import { renderToString } from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
+import serialize from 'serialize-javascript' // serialize: Replace special-characters with Unicode
 import Routes from '../client/Routes'
 
 /**
@@ -26,7 +27,7 @@ import Routes from '../client/Routes'
       <body>
         <div id="root">${content}</div>
         <script>
-          window.INITIAL_STATE = ${JSON.stringify(store.getState())}
+          window.INITIAL_STATE = ${serialize(store.getState())}
         </script>
         <script src="bundle.js"></script>
       </body>
